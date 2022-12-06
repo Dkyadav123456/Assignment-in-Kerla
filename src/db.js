bb7143d302e943c8cc7a167bea6d63e703ee4134
Dharmendra Yadav <you@example.com>
2022-12-06 11:17:57 +0530

    Backend

--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
-const mongoose =require("mongoose")
-const mongooseUri= "mongodb+srv://danish123:Faizan123450@cluster0.sk9syp9.mongodb.net/notebook"
-const connectToMongo =()=>{
-    mongoose.connect(mongooseUri, {
-        useNewUrlParser: true
+const mongoose = require("mongoose");
+const mongooseUri =
+  "mongodb+srv://Dharmendra:dkyadav123@cluster0.kq9bu.mongodb.net/Shopping-Cart";
+const connectToMongo = () => {
+  mongoose
+    .connect(mongooseUri, {
+      useNewUrlParser: true,
     })
-    .then(()=>console.log("mongoDB is connected"))
-    .catch((error)=>console.log(error))
-    
-}
-module.exports = connectToMongo
+    .then(() => console.log("mongoDB is connected"))
+    .catch((error) => console.log(error));
+};
+module.exports = connectToMongo;
